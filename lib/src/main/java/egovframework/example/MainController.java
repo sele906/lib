553f4fafffd093a3db079aa48f56e5d55c43c075
sele906 <sele906@gmail.com>
2024-10-15 15:29:53 +0900

    change main path

--- lib/src/main/java/egovframework/example/MainController.java
+++ lib/src/main/java/egovframework/example/MainController.java
@@ -1,5 +1,6 @@
 package egovframework.example;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,6 +8,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/main/*")
 public class MainController {
+
+	//서지정보 api 불러오기
+	@Value("${nl.KEY}")
+	private String nlApiKey;
 
 	@RequestMapping(value = "main.do", method = RequestMethod.GET)
 	public String main() throws Exception {
