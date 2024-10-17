5d366c9317e2ccbd0cffc14bde83c34d11115932
sele906 <sele906@gmail.com>
2024-10-17 09:49:21 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'main' of https://github.com/sele906/lib
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- lib/src/main/java/egovframework/example/MainController.java
+++ lib/src/main/java/egovframework/example/MainController.java
@@ -1,6 +1,5 @@
 package egovframework.example;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,10 +7,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/main/*")
 public class MainController {
-
-	//서지정보 api 불러오기
-	@Value("${nl.KEY}")
-	private String nlApiKey;
 
 	@RequestMapping(value = "main.do", method = RequestMethod.GET)
 	public String main() throws Exception {
